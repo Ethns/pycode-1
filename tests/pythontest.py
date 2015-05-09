bbd3d69f171cc1f8c84f70a921888540fe71d6b0
Ethan <shenye601@hotmail.com>
2015-05-09 22:33:00 +0800

    create tests folder

---
+++ tests/pythontest.py
@@ -0,0 +1,12 @@
+import unittest
+
+class PyUnitTestCases(unittest.TestCase):
+
+  def test_01_case01(self):
+    print """this is the first test case"""
+
+  def test_02_case02(self):
+    print """this is the second case"""
+
+if __name__ == '__main__':u
+  unittest.main()
